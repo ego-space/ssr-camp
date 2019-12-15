d32062f5b560ec2598ce230c8b9f77c2315556ef
GaryHjy <782316959@qq.com>
2019-12-15 16:29:57 +0800

    feat: 添加babel/polyfill配置

--- webpack.server.js
+++ webpack.server.js
@@ -22,8 +22,18 @@ module.exports = {
           presets: [
             '@babel/preset-react',
             [
-              '@babel/preset-env'
+              '@babel/preset-env',
+              {
+                'useBuiltIns': 'usage',
+                'corejs': {
+                  'version': 2
+                }
+              },
+              
             ]
+          ],
+          plugins: [
+            '@babel/plugin-transform-runtime'
           ]
         }
       }
