fddee38dc922349f9728ba0fc4117a4b485f720f
GaryHjy <782316959@qq.com>
2019-12-16 12:44:37 +0800

    feat: mock.js添加日志输出

--- mock.js
+++ mock.js
@@ -10,11 +10,24 @@ const start = (port) => {
 
 start(8888)
 
+// logger
+app.use(async (ctx, next) => {
+  const reqDate = Date.now()
+  const method = ctx.method
+  const path = ctx.path
+  await next()
+  const resDate = Date.now()
+  console.log(`${method} - ${path} - ${resDate - reqDate}ms`)
+
+})
+
 app.use((ctx) => {
+  // 处理同源策略请求 请求头
+  ctx.set("Access-Control-Allow-Origin", "*");
+  ctx.set("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
+  ctx.set("Content-Type", "application/json;charset=utf-8");
+
   if (ctx.path === '/api/course/list') {
-    ctx.set("Access-Control-Allow-Origin", "*");
-    ctx.set("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
-    ctx.set("Content-Type", "application/json;charset=utf-8");
     ctx.body = {
       code: 0, 
       list: [
@@ -35,6 +48,15 @@ app.use((ctx) => {
           id: 4,
         }
       ]
+    }
+  }
+  if (ctx.path === '/api/user/list') {
+    ctx.body = {
+      code: 0,
+      data: {
+        name: '小明',
+        id: 1
+      }
     }
   }
 })
