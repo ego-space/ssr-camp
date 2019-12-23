0c6efd47e49a8b9792f6d3669b5c71cd36c42377
GaryHjy <782316959@qq.com>
2019-12-24 00:06:24 +0800

    feat: 删除http文件夹

--- src/http/index.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-import { clientPort } from '../../config'
-// 创建axios实例，执行baseUrl
-const instance = axios.create({
-  baseURL: `http://localhost:${clientPort}`,
-  timeout: 1000,
-});
-
-export default instance
